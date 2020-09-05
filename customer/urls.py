86fb403e343915d3ef214086d1d9336bf40306f8
hpb1 <harry.prashantb@gmail.com>
2020-09-06 04:11:38 +0530

    Smaller changes

--- customer/urls.py
+++ customer/urls.py
@@ -3,5 +3,5 @@ from .views import StoreRequestView, CustomerRequestView
 urlpatterns = [
     path('request/create/<str:pk/', CustomerRequestView.as_view()), #store id
     path('request/delete/<str:pk/', CustomerRequestView.as_view()), #request id
-    path('request/view/<str:pk/', StoreRequestView.as_view()) #store id
+    path('request/view/<str:pk>/', StoreRequestView.as_view()) #store id
 ]
