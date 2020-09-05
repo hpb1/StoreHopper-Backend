d98a90f81cf1470df2025ac25ad9f80098551619
hpb1 <harry.prashantb@gmail.com>
2020-09-06 04:06:44 +0530

    Changes yet again

--- owner/urls.py
+++ owner/urls.py
@@ -3,8 +3,8 @@ from .views import StoreRegister, StoreProximity, StoreCityProximity
 
 urlpatterns = [
     path('add/', StoreRegister.as_view()),
-    path('modify/<int:pk>/', StoreRegister.as_view()),
-    path('delete/<int:pk>/', StoreRegister.as_view()),
+    path('modify/<str:pk>/', StoreRegister.as_view()),
+    path('delete/<str:pk>/', StoreRegister.as_view()),
     path('nearby/zipcode/', StoreProximity.as_view()),
     path('nearby/city/', StoreCityProximity.as_view()),
 ]
