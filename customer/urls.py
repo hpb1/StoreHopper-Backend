a93207c01e872f813565aeb2a869a36adfe1d684
hpb1 <harry.prashantb@gmail.com>
2020-09-06 04:03:33 +0530

    Change int to str

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import StoreRequestView, CustomerRequestView
 urlpatterns = [
-    path('request/create/<int:pk/', CustomerRequestView.as_view()), #store id
-    path('request/delete/<int:pk/', CustomerRequestView.as_view()), #request id
-    path('request/view/<int:pk/', StoreRequestView.as_view()) #store id
+    path('request/create/<str:pk/', CustomerRequestView.as_view()), #store id
+    path('request/delete/<str:pk/', CustomerRequestView.as_view()), #request id
+    path('request/view/<str:pk/', StoreRequestView.as_view()) #store id
 ]
