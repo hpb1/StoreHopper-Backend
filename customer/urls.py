62ea2cbbe711c4aa548f59f5b517ae3046ef0cad
hpb1 <harry.prashantb@gmail.com>
2020-09-06 12:00:58 +0530

    View for customer's requests

--- customer/urls.py
+++ customer/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('request/create/<str:pk>/', CustomerRequestView.as_view()), #store id
     path('request/delete/<str:pk>/', CustomerRequestView.as_view()), #itemrequest id
     path('request/view/<str:pk>/', StoreRequestView.as_view()), #store id
-    path('request/fulfill/<str:pk>/', StoreRequestView.as_view()) #itemrequest id
+    path('request/fulfill/<str:pk>/', StoreRequestView.as_view()), #itemrequest id
+    path('requests/customerview/', CustomerRequestView.as_view()) #customer id through header
 ]
