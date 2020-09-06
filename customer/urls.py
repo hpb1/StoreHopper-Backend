32d7376b7a5bc10cd911473f4187eae2e7981ced
hpb1 <harry.prashantb@gmail.com>
2020-09-06 11:42:12 +0530

    URL route update

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from .views import StoreRequestView, CustomerRequestView
 urlpatterns = [
-    path('request/create/<str:pk/', CustomerRequestView.as_view()), #store id
-    path('request/delete/<str:pk/', CustomerRequestView.as_view()), #itemrequest id
+    path('request/create/<str:pk>/', CustomerRequestView.as_view()), #store id
+    path('request/delete/<str:pk>/', CustomerRequestView.as_view()), #itemrequest id
     path('request/view/<str:pk>/', StoreRequestView.as_view()), #store id
     path('request/fulfill/<str:pk>/', StoreRequestView.as_view()) #itemrequest id
 ]
